5c985a8ccaa7b819f28bff68e87e9d69b53f408b
medcl <m@medcl.net>
2025-03-03 12:31:45 +0800

    add langchaingo

---
+++ src/github.com/tmc/langchaingo/llms/ernie/internal/ernieclient/ernieclient_test.go
@@ -0,0 +1,82 @@
+package ernieclient
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+	"strings"
+	"testing"
+)
+
+type mockHTTPClient struct{}
+
+// implement ernieclient.Doer interface.
+func (m *mockHTTPClient) Do(_ *http.Request) (*http.Response, error) {
+	authResponse := &authResponse{
+		AccessToken: "test",
+	}
+	b, err := json.Marshal(authResponse)
+	if err != nil {
+		return nil, err
+	}
+	response := &http.Response{
+		StatusCode: http.StatusOK,
+		Body:       io.NopCloser(strings.NewReader(string(b))),
+	}
+	return response, nil
+}
+
+func TestClient_buildURL(t *testing.T) {
+	t.Parallel()
+	type fields struct {
+		apiKey      string
+		secretKey   string
+		accessToken string
+		httpClient  Doer
+	}
+	type args struct {
+		modelpath ModelPath
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   string
+	}{
+		{
+			name: "with access token",
+			fields: fields{
+				accessToken: "token",
+			},
+			args: args{modelpath: "eb-instant"},
+			want: "https://aip.baidubce.com/rpc/2.0/ai_custom/v1/wenxinworkshop/chat/eb-instant?access_token=token",
+		},
+		{
+			name: "with client, aksk",
+			fields: fields{
+				apiKey:     "test",
+				secretKey:  "test",
+				httpClient: &mockHTTPClient{},
+			},
+			args: args{modelpath: "eb-instant"},
+			want: "https://aip.baidubce.com/rpc/2.0/ai_custom/v1/wenxinworkshop/chat/eb-instant?access_token=test",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			c, err := New(
+				WithAKSK(tt.fields.apiKey, tt.fields.secretKey),
+				WithAccessToken(tt.fields.accessToken),
+				WithHTTPClient(tt.fields.httpClient),
+			)
+			if err != nil {
+				t.Errorf("New got error. %v", err)
+			}
+			if got := c.buildURL(tt.args.modelpath); got != tt.want {
+				t.Errorf("buildURL() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
